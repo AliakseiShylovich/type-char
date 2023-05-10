89044cf3200f7255b0a246c59d7986f4d04a700f
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-10 14:40:39 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int k, s;
+        char a;
+        a = '*';
+        for ( k = 1; k<=3; k++) {
+
+            for (s = 1; s<=6; s++){
+                System.out.print(" " + a);
+            }
+                System.out.print( "\n");
+        }
+
+    }
+}
